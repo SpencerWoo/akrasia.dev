1c0b635af6d0e2488f8680133831ce77d07be6a5
C02CK13YMD6M <spencer.woo@liferay.com>
2022-12-05 23:37:02 -0800

    base cloudflare scripts

---
+++ scripts/cf/run-add.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+source .env
+
+# nvm install 10.13.0
+# nvm use 10.13.0
+
+rm -rf node_modules
+
+npm install &> /dev/null
+
+node cf-add-dns-records.js "$@"
+# --es-module-specifier-resolution=node
---
+++ scripts/cf/run-delete.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "$@"
+
+source .env
+
+# nvm install 10.13.0
+# nvm use 10.13.0
+
+rm -rf node_modules
+
+npm install &> /dev/null
+
+node cf-delete-dns-records.js "$@"
+# --es-module-specifier-resolution=node
