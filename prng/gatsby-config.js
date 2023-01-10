5e1b7152bdaa3641b1f684d74e27c8279b78ae0a
C02CK13YMD6M <spencer.woo@liferay.com>
2023-01-09 19:43:23 -0500

    init gatsby tutorial

---
+++ prng/gatsby-config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "My First Gatsby Site",
+  },
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
+    "gatsby-plugin-mdx",
+    "gatsby-transformer-sharp",
+  ],
+};
---
+++ prng/src/pages/index.js
@@ -0,0 +1,20 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
+import Seo from '../components/seo'
+
+const IndexPage = () => {
+  return (
+    <Layout pageTitle="Home Page">
+      <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, dozing in a bean bag chair"
+        src="../images/clifford.jpg"
+      />
+    </Layout>
+  )
+}
+
+export const Head = () => <Seo title="Home Page" />
+
+export default IndexPage
