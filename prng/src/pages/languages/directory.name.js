6773f44c1dc76b3a290525fb437214970974513a
C02CK13YMD6M <spencer.woo@liferay.com>
2023-01-15 16:06:45 -0500

    current functional base

---
+++ prng/src/pages/languages/directory.name.js
@@ -0,0 +1,92 @@
+import * as React from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
+import Layout from '../../components/layout'
+import Seo from '../../components/seo'
+
+const LanguagesPost = (props) => {
+  console.log(props)
+  const {pageContext, data} = props
+  const language = pageContext['name']
+  // console.log(data);
+
+  const nodes = data.allFile.nodes;
+
+  // overview language
+  // T1 image
+  // T1 result
+
+  // T2 image
+  // T2 result
+
+  // T3 image
+  // T3 result
+
+  // source
+
+  // node.name -- source_
+  // node.name -- trial
+  const imageNodes = nodes.filter(node => node.extension === 'jpg')
+  const sourceNode = nodes.filter(node => node.name === language)[0]
+  const resultNodes = nodes.filter(node => node.extension === '' || node.extension === 'txt')
+
+  console.log(imageNodes)
+  console.log(sourceNode)
+  console.log(resultNodes)
+
+  const images = imageNodes.map(node => getImage(node.childImageSharp))
+
+  // node.fields.contents.replaceAll(/\s/g, "\n")
+  return (
+    <Layout pageTitle={language}>
+
+      {images.map(image => 
+        <GatsbyImage
+          image={image}
+          alt=""
+        />
+      )}
+      <hr></hr>
+      
+      <pre>
+        <code>
+          {resultNodes.map(node => 
+            node.fields.contents
+          )}
+        </code>
+      </pre>
+
+      <hr></hr>
+
+      <pre>
+        <code>
+          {sourceNode.fields.contents}
+        </code>
+      </pre>
+        
+      <hr></hr>      
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query ($regLang: String){
+    allFile(filter: {relativePath: {regex: $regLang}}) {
+      nodes {
+        name
+        relativePath
+        extension
+        fields {
+          contents
+        }
+        childImageSharp {
+          gatsbyImageData
+        }
+      }
+    }
+  }
+`
+
+export const Head = ({ data }) => <Seo title='TODO' />
+
+export default LanguagesPost
--- prng/src/pages/languages/index.js
+++ prng/src/pages/languages/index.js
@@ -19,40 +19,8 @@ const LanguagesPage = ({ data }) => {
         ))
       }
     </Layout>
-    // <Layout pageTitle="My Blog Posts">
-    //   {
-    //     data.allMdx.nodes.map(node => (
-    //       <article key={node.id}>
-    //         <h2>
-    //           <Link to={`/blog/${node.frontmatter.slug}`}>
-    //             {node.frontmatter.title}
-    //           </Link>
-    //         </h2>
-    //         <p>Posted: {node.frontmatter.date}</p>
-    //         <p>{node.excerpt}</p>
-    //       </article>
-    //     ))
-    //   }
-    // </Layout>
   )
 }
-
-// export const query = graphql`
-//   query {
-//     allMdx(sort: { frontmatter: { date: DESC }}) {
-//       nodes {
-//         frontmatter {
-//           date(formatString: "MMMM D, YYYY")
-//           title
-//           slug
-//         }
-//         id
-//         excerpt
-//       }
-//     }
-//   }
-// `
-
 
 export const query = graphql`
   query {
