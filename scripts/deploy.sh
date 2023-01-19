7d29f1315bb8a253747bde2c93d6e686d08ee1a3
spencermwoo <spencermwoo>
2023-01-19 14:41:16 -0500

    generalize script for multiple websites

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
-BUCKET='akrasia.dev'
-gsutil rsync -R src gs://$BUCKET
+# ./deploy.sh akrasia.dev
+
+if [ -z "$1" ]
+  then
+    echo "No bucket supplied"
+    exit 1
+elif [ -z "$2" ]
+  then
+    echo "No localsrc supplied"
+    exit 1
+fi
+
+BUCKET=$1
+LOCALSRC=$2
+gsutil rsync -R $LOCALSRC gs://$BUCKET
 
 gsutil iam ch allUsers:objectViewer gs://$BUCKET
 
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,41 @@
 #!/bin/bash
 
+# ./scripts/setup.sh
+
+# BUCKET, BACKEND, DOMAIN, LOCALSRC
+# Assumptions:
+    # CF assumes BUCKET is name of subdomain
+
+# ./setup.sh prng prng-be akrasia.dev prng/public
+# ./setup.sh talon talon-be akrasia.dev talon
+
+if [ -z "$1" ]
+  then
+    echo "No bucket supplied"
+    exit 1
+elif [ -z "$2" ]
+  then
+    echo "No bucket backend supplied"
+    exit 1
+elif [ -z "$3" ]
+  then
+    echo "No domain supplied"
+    exit 1
+elif [ -z "$4" ]
+  then
+    echo "No src supplied"
+    exit 1
+fi
+
+BUCKET=$1
+BUCKET_BACKEND=$2
+DOMAIN=$3
+LOCALSRC=$4
+
+# echo $BUCKET
+# echo $BUCKET_BACKEND
+# echo $DOMAIN
+
 # https://cloud.google.com/load-balancing/docs/https/ext-load-balancer-backend-buckets#gcloudgsutil
 
 # 0)
@@ -8,15 +44,15 @@
 
 # 0)
 # auth GCP
-gcloud auth login
+# gcloud auth login
 
-PROJECT_ID=$(gcloud projects list --limit=1 --format 'value(project_id)')
-gcloud config set project $PROJECT_ID
+# PROJECT_ID=$(gcloud projects list --limit=1 --format 'value(project_id)')
+# gcloud config set project $PROJECT_ID
 
 # 1)
 # create storage bucket
-BUCKET='akrasia.dev'
-DOMAIN=$BUCKET
+# BUCKET='akrasia.dev'
+# DOMAIN=$BUCKET
 gcloud storage buckets create gs://$BUCKET
 
 # 2)
@@ -28,11 +64,11 @@ git pull origin
 # 3)
 # run ./deploy.sh
 # upload to bucket and set policy
-(cd scripts/cf && ./deploy.sh)
+(cd scripts/cf && ./deploy.sh $BUCKET $LOCALSRC)
 
 # 4)
 # create L7 LB
-BUCKET_BACKEND='akrasia'
+# BUCKET_BACKEND='akrasia'
 gcloud compute backend-buckets create $BUCKET_BACKEND \
     --gcs-bucket-name=$BUCKET
 
@@ -53,12 +89,12 @@ gcloud compute forwarding-rules create $BUCKET-lb-forwarding-rule \
 # 5)
 # delete existing rxecord
 (cd scripts/cf/ && ./run-delete.sh $DOMAIN $DOMAIN)
-(cd scripts/cf/ && ./run-delete.sh $DOMAIN 'talon')
+(cd scripts/cf/ && ./run-delete.sh $DOMAIN $BUCKET)
 
 # add new A record
 DNS_TYPE='A'
-IP_ADDR=$(gcloud compute forwarding-rules list --format 'value(IPAddress)')
+IP_ADDR=$(gcloud compute forwarding-rules list --filter name=$BUCKET-lb-forwarding-rule --format 'value(IPAddress)')
 
 (cd scripts/cf && ./run-add.sh $DOMAIN $IP_ADDR $DOMAIN $DNS_TYPE)
-(cd scripts/cf && ./run-add.sh $DOMAIN $IP_ADDR 'talon' $DNS_TYPE)
+(cd scripts/cf && ./run-add.sh $DOMAIN $IP_ADDR $BUCKET $DNS_TYPE)
 
