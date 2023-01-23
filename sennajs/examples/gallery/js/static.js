a5cee02093c79290c92dd5ae345ac9a1e919bc67
C02CK13YMD6M <spencer.woo@liferay.com>
2023-01-22 20:05:01 -0500

    remove static websites, host on spencers.dev instead

--- sennajs/examples/gallery/js/static.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-	var app = new senna.App();
-	app.setBasePath('/examples/gallery');
-	app.addSurfaces('preview');
-	app.addRoutes(new senna.Route(/\w+\.html/, senna.HtmlScreen));
-});
